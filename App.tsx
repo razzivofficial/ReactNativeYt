4c28be8e7e29f8f6df8f07036711a1503fc558f1
razzivofficial <razzivofficial@gmail.com>
2025-03-02 19:19:08 +0530

    basic-ui

--- App.tsx
+++ App.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+  StatusBar,
+  Alert,
+  Pressable,
+  SafeAreaView,
+} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#4CAF50" />
-      <Text style={styles.title}>Hello World</Text>
-      <Text style={styles.subtitle}>Welcome to React Native!</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Hellooo</Text>
+      <Image
+        style={{width: 200, height: 300, marginTop: 30}}
+        source={{
+          uri: 'https://images.pexels.com/photos/16470858/pexels-photo-16470858/free-photo-of-girls-in-apartment-windows.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+        }}
+      />
+
+      <Pressable
+        style={{padding: 10, backgroundColor: 'green'}}
+        onPress={() => Alert.alert('Presssed!')}>
+        <Text style={{color: 'white'}}>Button</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2C3E50',
-    padding: 20,
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: '#E74C3C',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 20,
-    color: '#ECF0F1',
-  },
-});
